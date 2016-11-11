3454a9f18e8e4424039e0d7760f6c0865022c935
usuario <usuario@10.22.143.47>
2016-11-11 09:59:57 +0000

    kata 5.1.1

---
+++ src/moneycalculator/ui/MoneyDisplay.java
@@ -0,0 +1,7 @@
+package moneycalculator.ui;
+
+public interface MoneyDisplay {
+    
+}
+
+
---
+++ src/moneycalculator/ui/swing/SwingMoneyDisplay.java
@@ -0,0 +1,24 @@
+package moneycalculator.ui.swing;
+
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import moneycalculator.ui.MoneyDisplay;
+
+public class SwingMoneyDisplay extends JPanel implements MoneyDisplay {
+    public SwingMoneyDisplay(){
+        setLayout(new FlowLayout());
+        add(amount());
+        add(currency());
+    }
+    
+    private JLabel amount(){
+        JLabel label = new JLabel("500");
+        return label;
+    }
+    
+    private JLabel currency(){
+        JLabel label = new JLabel ("$");
+        return label;
+    }
+}
